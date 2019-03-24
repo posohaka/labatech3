100736c2591820fb04ac51186f00e723ec1491e4
Opsy1169 <rodinilyaalexeevich@gmail.com>
2019-03-24 20:59:47 +0400

    добавил функцию выхода и кучу костылей, чтобы оно работало, не успел все дотестить

--- src/main/java/web/Server.java
+++ src/main/java/web/Server.java
@@ -66,6 +66,8 @@ public class Server {
                             }
                         }
 
+
+
                     } finally {
                         firstclient.close();
                         inFirst.close();
